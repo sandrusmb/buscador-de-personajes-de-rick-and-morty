48fd4acd5b51985cad633c7aa28ad0ac751a81c3
sandrusmb <sandrusmb@gmail.com>
2020-01-13 09:55:22 +0100

    Fix card styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../stylesheets/Card.scss";
 //QUITAR LA FOTO DE PRUEBA DE BILL MURRAY
-import murray from "../images/131x131.jpg";
+import example from "../images/example.jpg";
 
 function Card() {
   return (
     <li className="card">
-      <img className="card__photo" src={murray} alt="foto del personaje" />
+      <img className="card__photo" src={example} alt="foto del personaje" />
       <h3 className="card__name">Nombre</h3>
       <p className="card__type">Especie</p>
     </li>
