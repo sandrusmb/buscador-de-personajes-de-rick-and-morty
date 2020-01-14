626a0974ce8c28b1ea62be0551d39e44b65596e4
sandrusmb <sandrusmb@gmail.com>
2020-01-14 11:40:52 +0100

    Fix again docs folder

---
+++ docs/precache-manifest.b3cbf94b7d8ac80f3d5ed6e2f2d16ba8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767d2d37dec5d4a69e3088f18ecf3f18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1acd673b64d2329eabbc",
+    "url": "./static/css/main.d1ed2794.chunk.css"
+  },
+  {
+    "revision": "1608b8e0118375b4eec6",
+    "url": "./static/js/2.36206fc7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.36206fc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1acd673b64d2329eabbc",
+    "url": "./static/js/main.4e00b42f.chunk.js"
+  },
+  {
+    "revision": "c776b87109dc4c8c209c",
+    "url": "./static/js/runtime-main.62e6bced.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/rick.75509f2e.png"
+  },
+  {
+    "revision": "15cdb70ec6d438750a3a8b9e8acfeee5",
+    "url": "./static/media/universe2.15cdb70e.jpg"
+  }
+]);
